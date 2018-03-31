5519965d9bf8fdfaa57bea165c475ecd71b015b2
mp5maker <khan.photon@gmail.com>
2018-03-31 11:30:53 +0600

    31-March-2018, 11 29 AM
    31-March-2018, 11 29 AM

---
+++ Codes/AngularJS/8. Services/services.php
@@ -0,0 +1,14 @@
+<?php 
+$names = [
+			["name" => "Sam",
+			 "occupation"=> "Engineer"
+			],
+			  ["name" => "Bob", 
+			   "occupation"=> "Accountant"
+			],
+			   ["name" => "Mary",
+			    "occupation" => "Choreographer"
+			]
+		];
+echo json_encode($names);
+?>
